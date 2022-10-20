e383b39cc85afb5524e388bb22cbfba9b076fc3b
exectlyyy <ivanyburov@gmail.com>
2022-10-20 14:48:03 +0300

    V1.1 fixed
    ---

--- .venv/TelegramBotSchool.py
+++ .venv/TelegramBotSchool.py
@@ -3,25 +3,34 @@ import telebot
 from telebot import types
 
 bot = telebot.TeleBot('5670990101:AAHCY6UcN3pZC43P5FbVulljTAZVrlo4TWA');
-user_data = {}
+user_data = {}			#creating a dictionary with all user inputs to correct working
 
+#start function
 @bot.message_handler(commands=['start'])
 def greetings(message):
     bot.send_message(message.from_user.id, f'Привет, {message.from_user.first_name}, этот бот поможет тебе в решении задач по таким предметам как физика.')
 
+#help function
 @bot.message_handler(commands=['help'])
 def help(message):
-    bot.send_message(message.from_user.id, 'Проверка комманды /help, написать про предметы')
+    bot.send_message(message.from_user.id, 'Список команд:\n  /clear -  очищает актуальный кэш бота и возвращает к началу.')
 
+#clear function
+@bot.message_handler(commands=['clear'])
+def help(message):
+	global user_data
+	user_data[message.from_user.id] = []
+	bot.send_message(message.from_user.id, 'История очищена!')
+	
+#main function
 @bot.message_handler(content_types=['text'])			
 def main(message):
-	global user_data
+	global user_data			#setting local data to simplify the program
 	print(user_data)
 	if message.from_user.id not in user_data:
 		user_data[message.from_user.id] = []
 	local_data = user_data[message.from_user.id]
-	
-	if len(local_data) == 0:
+	if len(local_data) == 0:			#main branch
 		if message.text.lower() == 'предмет':
 			markup = types.ReplyKeyboardMarkup()    
 			subjbut1 = types.KeyboardButton('Физика') 			
@@ -30,7 +39,7 @@ def main(message):
 			local_data.append(message.text.lower())
 		else:
 			bot.send_message(message.from_user.id, 'Прости, я тебя не понимаю, для помощи напиши команду  /help')
-	elif len(local_data) == 1:
+	elif len(local_data) == 1:			#subject branch
 		if message.text.lower() == 'физика':
 			markup = types.ReplyKeyboardMarkup()
 			physchapter1 = types.KeyboardButton('Термодинамика')
@@ -39,7 +48,7 @@ def main(message):
 			local_data.append(message.text.lower())
 		else:
 			bot.send_message(message.from_user.id, 'Прости, я тебя не понимаю, выбери нужный тебе предмет:')
-	elif len(local_data) == 2:
+	elif len(local_data) == 2:			#chapter branch
 		if message.text.lower() == 'термодинамика':
 			markup = types.ReplyKeyboardMarkup()
 			pt1 = types.KeyboardButton('Количество теплоты')
@@ -48,26 +57,34 @@ def main(message):
 			local_data.append(message.text.lower())
 		else:
 			bot.send_message(message.from_user.id, 'Прости, я тебя не понимаю, выбери нужный тебе раздел:')
-	elif len(local_data) == 3:
-		if message.text.lower() == 'количество теплоты':
-			bot.send_message(message.from_user.id, "Введите удельную теплоёмкость (Дж/кг * °C)")
-			local_data.append(message.text.lower())
+	elif len(local_data) == 3 and message.text.lower() == 'количество теплоты':			#start of thermodynamic(1)
+		bot.send_message(message.from_user.id, "Введите удельную теплоёмкость (Дж/кг * °C)")
+		local_data.append(message.text.lower())
 	elif len(local_data) == 4 and local_data[3] == 'количество теплоты':
 		if int(message.text) > 0:
-			bot.send_message(message.from_user.id, "Введите начальную температуру")
+			bot.send_message(message.from_user.id, "Введите начальную температуру (°C)")
 			local_data.append(float(message.text.lower()))
+		else:
+			bot.send_message(message.from_user.id, "Введите корректное значение, если нужна помощь - /help")
 	elif len(local_data) == 5 and local_data[3] == 'количество теплоты':
-		if int(message.text) > 0:
-			bot.send_message(message.from_user.id, "Введите конечную  температуру")
+		if int(message.text) >= 0:
+			bot.send_message(message.from_user.id, "Введите конечную  температуру (°C)")
 			local_data.append(float(message.text.lower()))
+		else:
+			bot.send_message(message.from_user.id, "Введите корректное значение, если нужна помощь - /help")
 	elif len(local_data) == 6 and local_data[3] == 'количество теплоты':
-		if int(message.text) > 0:
-			bot.send_message(message.from_user.id, "Введите массу")
+		if int(message.text) >= 0:
+			bot.send_message(message.from_user.id, "Введите массу (кг)")
 			local_data.append(float(message.text.lower()))
+		else:
+			bot.send_message(message.from_user.id, "Введите корректное значение, если нужна помощь - /help")
 	elif len(local_data) == 7 and local_data[3] == 'количество теплоты':
 		local_data.append(float(message.text.lower()))
-		bot.send_message(message.from_user.id, f'Ответом на эту задачу является: {int(local_data[4] * (local_data[6] - local_data[5]) * local_data[7])} джоулей.')
-		user_data[message.from_user.id] = []
+		if int(local_data[4] * (local_data[6] - local_data[5]) * local_data[7]) % 1000 == 0:
+			bot.send_message(message.from_user.id, f'Ответом на эту задачу является: {int(local_data[4] * (local_data[6] - local_data[5]) * local_data[7]) // 1000} КДж.')
+		else:
+			bot.send_message(message.from_user.id, f'Ответом на эту задачу является: {int(local_data[4] * (local_data[6] - local_data[5]) * local_data[7])} Дж.')
+		user_data[message.from_user.id] = []				#end of thermodynamic(1)
 
 	print(user_data)
 
